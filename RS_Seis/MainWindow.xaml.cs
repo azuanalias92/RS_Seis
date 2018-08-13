c5716d248b089b08eddf46cc445f15b335f3e58c
Azuan Alias <azuanalias92@gmail.com>
2018-08-13 08:35:44 +0800

    add path and show file

--- RS_Seis/MainWindow.xaml.cs
+++ RS_Seis/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -36,19 +38,49 @@ namespace RS_Seis
         private void OpenDialog()
         {
             OpenFileDialog fileDialog = new OpenFileDialog();
-            fileDialog.DefaultExt = ".txt"; // Required file extension 
-            fileDialog.Filter = "Text documents (.txt)|*.txt"; // Optional file extensions
+            fileDialog.DefaultExt = ".dat"; // Required file extension 
+            fileDialog.Filter = "DAT file (.dat)|*.dat"; // Optional file extensions
 
-            fileDialog.ShowDialog();
-
-
-            /*if (fileDialog.ShowDialog() == DialogResult.OK)
+            if (fileDialog.ShowDialog() == true)
             {
-                System.IO.StreamReader sr = new
-                System.IO.StreamReader(fileDialog.FileName);
-                MessageBox.Show(sr.ReadToEnd());
-                sr.Close();
-            }*/
+                String filePath = fileDialog.FileName;
+                String dataValue = null;
+                int counter = 0;
+
+                path1.Text = filePath;
+
+                StreamReader objInput = new StreamReader(filePath, System.Text.Encoding.Default);
+                string contents = objInput.ReadToEnd().Trim();
+                string[] split = System.Text.RegularExpressions.Regex.Split(contents, "\\s+", RegexOptions.None);
+                foreach (string s in split)
+                {
+                    counter++;
+                    if(counter == 4)
+                    {
+                        dataValue = dataValue + s + "\n";
+                        Console.WriteLine(s);
+                    }else if (counter == 24)
+                    {
+                        counter = 0;
+                    }
+                    else
+                    {
+                        //default 
+                    }
+                    
+                }
+
+                box1.Text = dataValue;
+
+
+
+            }
+        }
+
+        private void box1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            textBox1.ScrollBars = ScrollBars.Both;
+            textBox1.WordWrap = false;
         }
     }
 }
